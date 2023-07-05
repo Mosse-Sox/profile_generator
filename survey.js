07ad2e7b8c1cbf0b0466b7db2bfff4d5ee14e881
Mosse-Sox <autumngreenie990@gmail.com>
2023-07-05 02:48:48 +0000

    survey.js asks a few questions and generates a paragraph about you based on those questions

--- survey.js
+++ survey.js
@@ -6,12 +6,12 @@ const rl = readline.createInterface({
 });
 
 const questions = [
-  "What is your name? Nicknames are also acceptable :) ",
-  "What is an activity you like doing? ",
-  "What do you listen to while doing that? ",
-  "Which meal is your favourite? (eg. dinner, brunch, etc.) ",
-  "Which sport is your absolute favourite? ",
-  "What is your superpower? In a few words, tell us what you are amazing at! ",
+  "1. What is your name? Nicknames are also acceptable :) ",
+  "2. What is an activity you like doing? ",
+  "3. What do you listen to while doing that? ",
+  "4. Which meal is your favourite? (eg. dinner, brunch, etc.) ",
+  "5. Which sport is your absolute favourite? ",
+  "6. What is your superpower? In a few words, tell us what you are amazing at! ",
 ];
 
 const answerGetter = (elm, callback) => {
@@ -27,7 +27,13 @@ const survey = (questions) => {
   
   const nextAnswer = () => {
     if (currentIndex === questions.length) {
-      console.log(answersArray);
+      console.log('================');
+      console.log(`
+      ${answersArray[0]} is someone who likes to ${answersArray[1]},
+      while they are listening to ${answersArray[2]}. ${answersArray[0]}'s favourite meal is
+      ${answersArray[3]}. ${answersArray[0]} is crazy about ${answersArray[4]}. 
+      The juiciest thing about ${answersArray[0]} is that their superpower is ${answersArray[5]}.
+      `);
       rl.close();
       return;
     }
